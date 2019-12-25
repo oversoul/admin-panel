dc757510a38b3dc8a4b1b84b7604e9fab367d395
Amine <kurosoul.ae@gmail.com>
2019-12-25 18:33:29 +0100

    adding dashboard cards

---
+++ src/Card.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Aecodes\AdminPanel;
+
+class Card
+{
+
+    /**
+     * Form width
+     *
+     * @var integer
+     */
+    protected $size = 100;
+
+    /**
+     * Title of the card
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * Content of the card
+     *
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * Allowed sizes
+     *
+     * @var array
+     */
+    protected $allowedSizes = [25, 50, 75, 100];
+
+    /**
+     * Undocumented function
+     *
+     * @param string $title
+     * @param string $content
+     */
+    public function __construct(string $title, string $content)
+    {
+        $this->title = $title;
+        $this->content = $content;
+    }
+
+    /**
+     * Create form instance statically
+     *
+     * @param sting $title
+     * @param sting $content
+     * @return self
+     */
+    public static function make(string $title, string $content): self
+    {
+        return new static($title, $content);
+    }
+
+    /**
+     * Set form size
+     *
+     * @param integer $percent
+     * @return self
+     */
+    public function size(int $percent): self
+    {
+        if (!in_array($percent, $this->allowedSizes)) {
+            $percent = 100;
+        }
+
+        $this->size = $percent;
+        return $this;
+    }
+
+    protected function getColumnSize(): string
+    {
+        switch ($this->size) {
+            case 25:
+                return 'col-3';
+            case 50:
+                return 'col-6';
+            case 75:
+                return 'col-9';
+            default:
+                return 'col-12';
+        }
+    }
+
+    /**
+     * Build form
+     *
+     * @param mixed $source
+     * @return string
+     */
+    public function build($source): string
+    {
+        $title = $this->title;
+        $content = $this->content;
+        $size = $this->getColumnSize();
+
+        ob_start();
+        require dirname(__FILE__) . "/presenters/card.php";
+        return ob_get_clean();
+    }
+}
---
+++ src/presenters/card.php
@@ -0,0 +1,10 @@
+<div class="col-3">
+    <div class="card">
+        <div class="card-header">
+            <?= $title ?>
+        </div>
+        <div class="card-body">
+            <?= $content ?>
+        </div>
+    </div>
+</div>
