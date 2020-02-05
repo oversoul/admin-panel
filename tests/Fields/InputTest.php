fa7dfce151396c61d0b6434c6aa3b23572483455
Amine <kurosoul.ae@gmail.com>
2020-02-05 12:58:11 +0100

    adding test cases for input and select

---
+++ tests/Fields/InputTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Aecodes\Tests\Fields;
+
+use PHPUnit\Framework\TestCase;
+use Aecodes\AdminPanel\Dashboard;
+use Aecodes\AdminPanel\AdminConfig;
+use Aecodes\AdminPanel\Fields\Input;
+use Aecodes\AdminPanel\Layouts\View;
+
+class InputTest extends TestCase
+{
+
+    protected $view;
+
+    public function setUp(): void
+    {
+        $config = new class extends AdminConfig {};
+
+        Dashboard::make($config);
+
+        $this->view = new View;
+        $this->view->page = new class {};
+    }
+
+    public function testInputHasDefaultTextType()
+    {
+        $input = Input::make('about')->build([], $this->view);
+
+        $this->assertStringContainsString('type="text"', $input);
+        $this->assertStringContainsString('about', $input);
+    }
+
+    public function testInputTypeCanBeCustomized()
+    {
+        $input = Input::email('about')->build([], $this->view);
+
+        $this->assertStringContainsString('type="email"', $input);
+        $this->assertStringContainsString('about', $input);
+    }
+
+    public function testInputTypeCanBeNumber()
+    {
+        $input = Input::number('about')->build([], $this->view);
+
+        $this->assertStringContainsString('type="number"', $input);
+        $this->assertStringContainsString('about', $input);
+    }
+
+    public function testInputCanHaveAttributes()
+    {
+        $input = Input::text('about')
+            ->class('text-class')
+            ->build([], $this->view);
+
+        $this->assertStringContainsString('type="text"', $input);
+        $this->assertStringContainsString('class="text-class"', $input);
+        $this->assertStringContainsString('about', $input);
+    }
+
+}
---
+++ tests/Fields/SelectText.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Aecodes\Tests\Fields;
+
+use PHPUnit\Framework\TestCase;
+use Aecodes\AdminPanel\Dashboard;
+use Aecodes\AdminPanel\AdminConfig;
+use Aecodes\AdminPanel\Layouts\View;
+use Aecodes\AdminPanel\Fields\Select;
+
+class SelectTest extends TestCase
+{
+
+    protected $view;
+
+    public function setUp(): void
+    {
+        $config = new class extends AdminConfig {};
+
+        Dashboard::make($config);
+
+        $this->view = new View;
+        $this->view->page = new class {};
+    }
+
+    public function testSelectCanBeCreated()
+    {
+        $input = Select::make('about')->title('About')->build([], $this->view);
+
+        $this->assertStringContainsString('about', $input);
+        $this->assertStringContainsString('About', $input);
+    }
+
+    public function testSelectCanHasOptions()
+    {
+        $input = Select::make('about')->title('About')->options([
+            1, 2, 3, 4
+        ])->build([], $this->view);
+
+        $this->assertStringContainsString('about', $input);
+        $this->assertStringContainsString('About', $input);
+        $this->assertStringContainsString(1, $input);
+        $this->assertStringContainsString(2, $input);
+        $this->assertStringContainsString(3, $input);
+        $this->assertStringContainsString(4, $input);
+    }
+
+}
