feb406b1f28588a3be0bb00582c05cb01fb3ab1a
Amine <kurosoul.ae@gmail.com>
2020-03-13 22:13:00 +0100

    ui: remove some fields background color

--- presenters/fields/textarea.php
+++ presenters/fields/textarea.php
@@ -4,7 +4,7 @@
   <textarea
   	id="<?= $name ?>"
   	name="<?= $name ?>"
-	class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+	class="appearance-none block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
   	<?= $attributes ?>
   ><?= $value ?></textarea>
 </div>
