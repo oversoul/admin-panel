4a05ae78b66bc8e73014bf7c20b44c0bdf8c7521
Amine <kurosoul.ae@gmail.com>
2020-11-16 13:39:22 +0100

    radio: fix for checked state

--- src/Widgets/Fields/Checkbox.php
+++ src/Widgets/Fields/Checkbox.php
@@ -2,14 +2,54 @@
 
 namespace Aecodes\AdminPanel\Widgets\Fields;
 
-class Checkbox extends Input
+use Aecodes\AdminPanel\Helper;
+use Aecodes\AdminPanel\Dashboard;
+
+class Checkbox extends Field
 {
 
-    protected $type = 'checkbox';
+	/**
+	 * @return $this
+	 */
+	public function checked(): self
+	{
+		$this->attributes['checked'] = 'checked';
+		return $this;
+	}
 
-    protected function getAttributes(): array
-    {
-        $this->attributes = ['checked' => $this->value];
-        return parent::getAttributes();
-    }
+	protected function isChecked(array $data): bool
+	{
+		if ($this->noFill || !$this->value) {
+			return false;
+		}
+
+		$value = Helper::arr_get($data, $this->target, false);
+		$value = Dashboard::oldValue($this->target, $value);
+
+		return ($value === $this->value);
+	}
+
+	public function build(array $data): array
+	{
+		$this->getValue($data);
+
+		$attributes = [
+			'type'  => 'checkbox',
+			'name'  => $this->name,
+			'value' => $this->value,
+		];
+
+		if ($this->isChecked($data)) {
+			$attributes['checked'] = 'checked';
+		}
+
+		$attributes = array_merge($this->getAttributes(), $attributes);
+
+		return [
+			'type'       => 'Checkbox',
+			'title'      => $this->title,
+			'help'       => $this->help,
+			'attributes' => $attributes,
+		];
+	}
 }
--- tests/Fields/CheckboxTest.php
+++ tests/Fields/CheckboxTest.php
@@ -2,9 +2,9 @@
 
 namespace Aecodes\AdminPanel\Tests\Fields;
 
-use Aecodes\AdminPanel\Widgets\Fields\Checkbox;
-use Aecodes\AdminPanel\Widgets\Fields\Input;
 use PHPUnit\Framework\TestCase;
+use Aecodes\AdminPanel\Widgets\Fields\Input;
+use Aecodes\AdminPanel\Widgets\Fields\Checkbox;
 
 class CheckboxTest extends TestCase
 {
@@ -15,8 +15,7 @@ class CheckboxTest extends TestCase
         $input = Checkbox::make('checkbox')->title('About')->build([]);
 
         $this->assertArrayHasKey('type', $input);
-        $this->assertEquals('fields/input', $input['type']);
-        $this->assertEquals('About', $input['title']);
+        $this->assertEquals('Checkbox', $input['type']);
         $this->assertEquals('checkbox', $input['attributes']['type']);
     }
 
@@ -26,8 +25,17 @@ class CheckboxTest extends TestCase
         $input = Input::checkbox('checkbox')->title('About')->build([]);
 
         $this->assertArrayHasKey('type', $input);
-        $this->assertEquals('fields/input', $input['type']);
-        $this->assertEquals('About', $input['title']);
+        $this->assertEquals('Checkbox', $input['type']);
         $this->assertEquals('checkbox', $input['attributes']['type']);
     }
+
+	/** @test */
+	public function checkboxTesting()
+	{
+		$input = Input::checkbox('online')->title('Online?')->build(['online' => true]);
+		$this->assertArrayHasKey('checked', $input['attributes']);
+
+		$input = Input::checkbox('online')->title('Online?')->build(['online' => false]);
+		$this->assertArrayNotHasKey('checked', $input['attributes']);
+	}
 }
